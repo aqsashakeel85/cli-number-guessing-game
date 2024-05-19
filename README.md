238ca2b2a765470027a697980cd3166037122ea3
aqsashakeel85 <167136620+aqsashakeel85@users.noreply.github.com>
2024-05-19 14:19:03 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# cli-number-guessing-game
-This is a cli based number guessing game
+# number-guessing-game
+cli based number guessing game
---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("\n\twellcome to codewithAqsa - CLI Number Guessing Game\n");
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessNumber",
+        type: "number",
+        message: "Enter your guess number(Number Limit from 1 to 5):",
+    },
+]);
+if (answer.userGuessNumber === randomNumber) {
+    console.log("Congratulations ! you guess a correct number");
+}
+else {
+    console.log("sorry , you guess a wrong number");
+}
